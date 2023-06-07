f1672ab2643e78c420e1e73a62899d01dbbc6d92
solituude <dbelousova70@gmail.com>
2023-06-08 00:55:48 +0400

    Таблица с данными v1

--- registration-of-applications/src/App.js
+++ registration-of-applications/src/App.js
@@ -1,8 +1,8 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/NavBar/NavBar';
 import { Container } from "react-bootstrap";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavBar from './Components/NavBar/NavBar';
 import Statement from "./Components/Statement/Statement";
 import StatementAdd from "./Components/StatementAdd/StatementAdd";
 import StatementEdit from "./Components/StatementEdit/StatementEdit";
@@ -13,13 +13,12 @@ function App() {
     <Container fluid className="main__content">
         <BrowserRouter>
             <NavBar />
-
             <Routes>
-                <Route src='/' element={<Statement/>}/>
-                <Route src='/statement' element={<Statement/>}/>
-                <Route src='/statement/add' element={<StatementAdd />}/>
-                <Route src='/statement/edit' element={<StatementEdit/>}/>
-                <Route src='/map' element={<Map/>} />
+                <Route path='/' element={<Statement/>}/>
+                <Route path='/statement' element={<Statement/>}/>
+                <Route path='/statement/add' element={<StatementAdd />}/>
+                <Route path='/statement/edit' element={<StatementEdit/>}/>
+                <Route path='/map' element={<Map/>} />
             </Routes>
         </BrowserRouter>
     </Container>
--- registration-of-applications/src/Components/Statement/Statement.jsx
+++ registration-of-applications/src/Components/Statement/Statement.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Container, Table} from "react-bootstrap";
+import someData from '../../data/MOCK_DATA-2.json';
 
 const Statement = () => {
-    return(
-        <div>
+    const [data, setData] = useState([]);
 
-        </div>
+    useEffect(() => {
+        setData(someData);
+    }, [])
+
+    return(
+        <Container>
+            <Table>
+                <thead>
+                    <th>Номер заявки</th>
+                    <th>Адрес</th>
+                    <th>Тип аварии</th>
+                    <th>Приоритет</th>
+                    <th>Заявитель</th>
+                    <th>Номер телефона</th>
+                    <th>Действия</th>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Ростов-на-Дону, ул. Ленина, 36</td>
+                        <td>прорыв</td>
+                        <td>незамедлительно</td>
+                        <td>Иванов Иван</td>
+                        <td>+7(987)-673-9876</td>
+                        <td>delete</td>
+                    </tr>
+
+                    {data.map((item) => (
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.address}</td>
+                            <td>{item.type}</td>
+                            <td>{item.priority}</td>
+                            <td>{item.name}</td>
+                            <td>{item.phone}</td>
+                            <td>delete</td>
+                        </tr>
+                    ))}
+
+
+
+                </tbody>
+
+
+            </Table>
+        </Container>
     );
 }
 
