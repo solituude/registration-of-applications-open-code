dfa16b00be1c652ece590024f73730f7b31fbb74
solituude <dbelousova70@gmail.com>
2023-06-07 18:39:17 +0400

    navbar v1

--- registration-of-applications/src/App.js
+++ registration-of-applications/src/App.js
@@ -1,12 +1,28 @@
-
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
+import { Container } from "react-bootstrap";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Statement from "./Components/Statement/Statement";
+import StatementAdd from "./Components/StatementAdd/StatementAdd";
+import StatementEdit from "./Components/StatementEdit/StatementEdit";
+import Map from "./Components/Map/Map";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <Container fluid className="main__content">
+        <BrowserRouter>
+            <NavBar />
+
+            <Routes>
+                <Route src='/' element={<Statement/>}/>
+                <Route src='/statement' element={<Statement/>}/>
+                <Route src='/statement/add' element={<StatementAdd />}/>
+                <Route src='/statement/edit' element={<StatementEdit/>}/>
+                <Route src='/map' element={<Map/>} />
+            </Routes>
+        </BrowserRouter>
+    </Container>
   );
 }
 
--- registration-of-applications/src/Components/NavBar/NavBar.jsx
+++ registration-of-applications/src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import s from './navbar.module.scss';
+import {Navbar} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+
+import lightThemeToggleIcon from '../../img/ligthThemeIcon.svg';
 
 const NavBar = () => {
     return(
-        <ul className={s.list}>
-            <li className={s.navbar__item}>Журнал</li>
-            <li className={s.navbar__item}>Карта</li>
-        </ul>
+        <Navbar className={s.nav__container}>
+                <div className={s.toggle__theme}>
+                    <img src={lightThemeToggleIcon} alt="светлая тема" />
+                </div>
+                <NavLink to='/statement' className={s.nav__item}>Журнал</NavLink>
+                <NavLink to='/map' className={s.nav__item}>Карта</NavLink>
+
+        </Navbar>
     );
 }
 
