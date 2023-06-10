242c4413600c19336ef9da9790fbf7d318afbe04
solituude <dbelousova70@gmail.com>
2023-06-11 03:14:57 +0400

    add themeProvider

--- registration-of-applications/src/Components/NavBar/NavBar.jsx
+++ registration-of-applications/src/Components/NavBar/NavBar.jsx
@@ -4,17 +4,28 @@ import {Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 
 import lightThemeToggleIcon from '../../img/ligthThemeIcon.svg';
+import {ThemeContext, themes} from "../../contexts/ThemeContext";
 
 const NavBar = () => {
     return(
-        <Navbar className={s.nav__container}>
-                <div className={s.toggle__theme}>
-                    <img src={lightThemeToggleIcon} alt="светлая тема" />
-                </div>
-                <NavLink to='/statement' className={s.nav__item}>Журнал</NavLink>
-                <NavLink to='/map' className={s.nav__item}>Карта</NavLink>
+        <ThemeContext.Consumer>
+            {({theme, setTheme}) => (
+                <Navbar className={s.nav__container}>
+                    <div className={s.toggle__theme} onClick={() => {
+                        if (theme === themes.light) setTheme(themes.dark)
+                        if (theme === themes.dark) setTheme(themes.light)
+                    }
+                    }>
+                        <img src={lightThemeToggleIcon} alt="светлая тема" />
+                    </div>
+                    <NavLink to='/statement' className={s.nav__item}>Журнал</NavLink>
+                    <NavLink to='/map' className={s.nav__item}>Карта</NavLink>
 
-        </Navbar>
+                </Navbar>
+            )}
+
+        </ThemeContext.Consumer>
+
     );
 }
 
--- registration-of-applications/src/Components/Statement/Statement.jsx
+++ registration-of-applications/src/Components/Statement/Statement.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {Container, Table} from "react-bootstrap";
-import someData from '../../data/MOCK_DATA-2.json';
+import someData from '../../data/MOCK_DATA-3.json';
+import { Tag } from 'antd';
 
 const Statement = () => {
     const [data, setData] = useState([]);
@@ -23,33 +24,22 @@ const Statement = () => {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Ростов-на-Дону, ул. Ленина, 36</td>
-                        <td>прорыв</td>
-                        <td>незамедлительно</td>
-                        <td>Иванов Иван</td>
-                        <td>+7(987)-673-9876</td>
-                        <td>delete</td>
-                    </tr>
-
                     {data.map((item) => (
                         <tr>
                             <td>{item.id}</td>
                             <td>{item.address}</td>
-                            <td>{item.type}</td>
-                            <td>{item.priority}</td>
+                            <td>{item.accident}</td>
+                            <td>{
+                                item.priority === "Незамедлительно" ? <Tag color="red"> Незамедлительно</Tag> :
+                                    item.priority === "Высокий" ? <Tag color="volcano">Высокий</Tag> :
+                                        item.priority === "Средний" ? <Tag color="gold">Средний</Tag> :
+                                            <Tag color="green">Низкий</Tag>}</td>
                             <td>{item.name}</td>
                             <td>{item.phone}</td>
                             <td>delete</td>
                         </tr>
                     ))}
-
-
-
                 </tbody>
-
-
             </Table>
         </Container>
     );
